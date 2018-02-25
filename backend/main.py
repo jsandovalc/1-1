8f2cdfe7f368af05fa03de3146b7668c71cd5e73
Jonathan Sandoval Cardona <ark@gaia>
2018-02-24 23:06:37 -0500

    UI Mock

--- backend/main.py
+++ backend/main.py
@@ -8,3 +8,8 @@ app.static('/static', './dist/static')
 @app.route('/')
 async def index(request):
     return await file('dist/index.html')
+
+@app.route('/parse', methods=['POST'])
+async def parse(request):
+    """Parse expression, return"""
+    return json({'message': 'empty'})
---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <div id="input">
+          <textarea v-model="expr" v-on:input="parseExpr"></textarea>
+    </div>
+    <div id="output">
+      <table>
+        <thead>
+          <tr>
+            <th>A</th>
+            <th>B</th>
+            <th>C</th>
+          </tr>
+        </thead>
+        <tbody>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data: function () {
+    return {
+      expr: ''
+    }
+  },
+  methods: {
+    parseExpr: function () {
+      axios.post('/parse', {
+        expr: '1 + 1'
+      }).then(function (response) {
+        console.log(response)
+      }).catch(function (error) {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
