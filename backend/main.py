7c9207d7448c1fc90b341c1c5ba55c7b7ecc332d
Jonathan Sandoval Cardona <ark@gaia>
2018-03-17 17:37:08 -0500

    Add visitor to polish notation.

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+import ast
 from sanic import Sanic
 from sanic.response import json, file
 
@@ -16,6 +17,49 @@ async def parse(request):
     Request must include `expr` and `table`.
 
     """
-    print(request.json)
     return json({'result': eval(request.json['expr'],
                                 request.json['table'])})
+
+
+
+class PolishNotationVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.pn = []
+
+        super().__init__()
+
+    def visit_BinOp(self, node):
+        self.visit(node.op)
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_Add(self, node):
+        self.pn.append(dict(label='+', type='binop'))
+
+    def visit_Sub(self, node):
+        self.pn.append(dict(label='-', type='binop'))
+
+    def visit_Mult(self, node):
+        self.pn.append(dict(label='*', type='binop'))
+
+    def visit_Div(self, node):
+        self.pn.append(dict(label='/', type='binop'))
+
+    def visit_Pow(self, node):
+        self.pn.append(dict(label='^', type='binop'))
+
+    def visit_Num(self, node):
+        self.pn.append(dict(label=node.n, type='num'))
+
+
+@app.route('/display')
+async def display(request):
+    """Return a json to display in client.
+
+    Request must include `expr`.
+    """
+    tree = ast.parse(request.json['expr'])
+    visitor = PolishNotationVisitor()
+    visitor.visit(tree)
+
+    return json(visitor.pn)
