76bb2c83d56b19fe741b364321d805c620e7551d
Jonathan Sandoval Cardona <ark@gaia>
2018-03-17 17:37:20 -0500

    Add snap graphic space.

--- src/GraphArea.vue
+++ src/GraphArea.vue
@@ -1,5 +1,5 @@
 <template>
-<svg id="svg" style="width: 300px; height: 300px; border-radius: 10px; border: solid 2px #ccc">
+<svg id="svg">
 
 </svg>
 </template>
@@ -9,12 +9,23 @@ import Snap from 'snapsvg'
 
 export default {
   name: 'InputCommand',
+  data: function () {
+    return {
+      snap: null
+    }
+  },
   mounted: function () {
     var snap = Snap('#svg')
-    snap.circle(150, 150, 100)
+    this.snap = snap
   }
 }
 </script>
 
 <style scoped>
+svg {
+  width: 300px;
+  height: 300px;
+  border-radius: 10px;
+  border: solid 2px #ccc
+}
 </style>
