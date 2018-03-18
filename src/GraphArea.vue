2ee61348e41e9aecf386db85d352f616571d8e2c
Jonathan Sandoval Cardona <ark@gaia>
2018-03-18 09:37:23 -0500

    Draw exprs in graph area.

--- src/GraphArea.vue
+++ src/GraphArea.vue
@@ -19,28 +19,31 @@ export default {
     var snap = Snap('#svg')
     this.snap = snap
   },
-  watch: {
-    expr: function (newExpr, oldExpr) {
-      // must clear canvas. Redraw on new expression.
-      this.snap.clear()
-      let self = this
-
-      let x = 10
-      let y = 10
-      let width = 100
-      let height = 50
-
-      newExpr.forEach(function (value, index) {
-        let rect = self.snap.rect(x, y, width, height)
-        rect.attr({
-          stroke: 'black',
-          strokewidth: 5,
-          fill: 'white'})
-
-        self.snap.text((x + width) / 2,
-                       (y + height) / 2 + 10,
-                       value.label)
+  methods: {
+    drawTree: function (tree, x, y) {
+      let rect = this.snap.rect(x, y, 100, 50)
+      rect.attr({
+        stroke: 'black',
+        strokewidth: 5,
+        fill: 'white'
       })
+      this.snap.text(x + 100 / 2,
+                     y + 50 / 2 + 10,
+                     tree.label)
+      if ('left' in tree) {
+        this.drawTree(tree.left, x, y + 50 + 50)
+      }
+
+      if ('right' in tree) {
+        this.drawTree(tree.right, x + 100 + 50, y)
+      }
+    }
+  },
+  watch: {
+    expr: function (newRoot, oldRoot) {
+      this.snap.clear()
+
+      this.drawTree(newRoot, 10, 10)
     }
   }
 }
